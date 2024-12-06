504d49acfab8aecc0e6981294b20e8cadf55a8f4
Alex Bepple <alex@bepple.de>
2024-12-06 15:13:23 +0100

    (git) $ make it easier to get side-by-side diff in terminal

--- dotconfig/fish/plugins/git/conf.d/git.fish
+++ dotconfig/fish/plugins/git/conf.d/git.fish
@@ -6,7 +6,12 @@ abbr gc git commit --verbose
 abbr gac 'git add -A :/; and git commit --verbose'
 
 abbr gs git status
-abbr gd git diff --histogram --word-diff=color
+
+set _gds "git diff"
+abbr gdu git diff --histogram --word-diff=color
+abbr gds $_gds
+abbr gd $_gds
+
 abbr gl git log --oneline --decorate --all --graph -12
 
 abbr gwR git reset --hard
